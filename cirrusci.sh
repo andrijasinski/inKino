c2853971f61204c932e032c9aac8585b0c18ba29
andrijasinski <andri.jasinski@gmail.com>
2019-05-10 15:13:45 +0300

    get flutter via git

--- cirrusci.sh
+++ cirrusci.sh
@@ -6,10 +6,12 @@ export CLONE_DIR=`pwd`
 cd core/lib/src && mv tmdb_config.dart.sample tmdb_config.dart && cd ../../..
 
 cd ~
-wget https://github.com/flutter/flutter/archive/v1.5.4-hotfix.2.tar.gz
-tar -xzf v1.5.4-hotfix.2.tar.gz
-cd flutter-1.5.4-hotfix.2
+
+git clone -b stable https://github.com/flutter/flutter.git
+cd flutter
 export PATH="$PATH:`pwd`/bin"
 
 cd $CLONE_DIR
+flutter --version
+flutter packages pub get
 flutter build ios --no-codesign --release -v
